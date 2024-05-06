322431f4fc6f9b4ac2a286e14d015201cab41f2e
FrancoisLGRND <francois.lgnd@gmail.com>
2024-05-06 16:13:07 +0200

    fix css

--- resources/views/appartements/index.blade.php
+++ resources/views/appartements/index.blade.php
@@ -1,7 +1,11 @@
 <x-app-layout>
+<div class="flex justify-center">
+    <div class="grid grid-cols-6 gap-6 w-9/12">
+        @include('appartements.partials.appartement-sorting')
+    </div>
+    </div>
     <div class="flex justify-center">
         <div class="grid grid-cols-6 gap-6 w-9/12">
-            @include('appartements.partials.appartement-sorting')
             @forelse ($appartements as $appartement)
 
                 <div class="mt-9">
