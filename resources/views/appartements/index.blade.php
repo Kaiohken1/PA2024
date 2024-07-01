2235e0faabe60372c6cead2bcb6271e6360b5419
Kaioken <raygsm@live.fr>
2024-07-01 11:57:03 +0200

    ajout lib langues

--- resources/views/appartements/index.blade.php
+++ resources/views/appartements/index.blade.php
@@ -27,17 +27,17 @@
                                 <img class="rounded-md w-full aspect-square" src="{{ Storage::url($appartement->images->first()->image) }}"
                                     >
                             @else
-                                <p>Aucune image disponible</p>
+                                <p>{{__('Aucune image disponible')}}</p>
                             @endif
                             
                                     <h1 class="text-2xl font-extrabold">{{ $appartement->name }}</h1>
                                     <p>{{ $appartement->address }}</p>
-                                    <p>Loué par {{ $appartement->user->name }}</p>
+                                    <p>{{__('Loué par')}} {{ $appartement->user->name }}</p>
                                     
                                         
-                                    <p><span class="font-extrabold">{{ $appartement->price }}€</span> par nuit</p>
+                                    <p><span class="font-extrabold">{{ $appartement->price }}€</span>{{__('par nuit')}} </p>
                                 
-                                    <span class="font-extrabold size-max inline-flex"><x-ri-star-fill class="size-6"/>{{ $appartement->overall_rating }} | {{ $appartement->avis_count }} Avis</span>
+                                    <span class="font-extrabold size-max inline-flex"><x-ri-star-fill class="size-6"/>{{ $appartement->overall_rating }} | {{ $appartement->avis_count }}{{__('Avis')}}</span>
                                     <div>
                             @foreach ($appartement->tags as $tag)
                                 <span
@@ -51,9 +51,9 @@
                 <div class="py-12">
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                         <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg flex flex-col items-center">
-                            <p class="text-center text-gray-600 text-lg">Aucun appartement disponible...</p>
-                            <x-primary-button class="mt-4"><a href="{{ route('property.create') }}">Et si vous
-                                    proposiez le vôtre ?</a></x-primary-button>
+                            <p class="text-center text-gray-600 text-lg">{{__('Aucun appartement disponible...')}}</p>
+                            <x-primary-button class="mt-4"><a href="{{ route('property.create') }}">{{__('Et si vous
+                                    proposiez le vôtre ?')}}</a></x-primary-button>
                         </div>
                     </div>
                 </div>
